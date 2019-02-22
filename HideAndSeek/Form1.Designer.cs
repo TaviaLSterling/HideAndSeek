69a5d7137a64806b14cedd93f8fb43c476ddb5ba
Tavia Sterling <TaviaSterling@outlook.com>
2019-02-22 11:22:46 -0700

    hide and check buttons

--- HideAndSeek/Form1.Designer.cs
+++ HideAndSeek/Form1.Designer.cs
@@ -32,6 +32,8 @@
             this.goHere = new System.Windows.Forms.Button();
             this.goThroughTheDoor = new System.Windows.Forms.Button();
             this.exits = new System.Windows.Forms.ComboBox();
+            this.check = new System.Windows.Forms.Button();
+            this.hide = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // description
@@ -44,7 +46,7 @@
             // 
             // goHere
             // 
-            this.goHere.Location = new System.Drawing.Point(25, 156);
+            this.goHere.Location = new System.Drawing.Point(25, 126);
             this.goHere.Name = "goHere";
             this.goHere.Size = new System.Drawing.Size(75, 23);
             this.goHere.TabIndex = 1;
@@ -53,7 +55,7 @@
             // 
             // goThroughTheDoor
             // 
-            this.goThroughTheDoor.Location = new System.Drawing.Point(72, 202);
+            this.goThroughTheDoor.Location = new System.Drawing.Point(43, 156);
             this.goThroughTheDoor.Name = "goThroughTheDoor";
             this.goThroughTheDoor.Size = new System.Drawing.Size(171, 23);
             this.goThroughTheDoor.TabIndex = 2;
@@ -64,16 +66,36 @@
             // 
             this.exits.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.exits.FormattingEnabled = true;
-            this.exits.Location = new System.Drawing.Point(122, 156);
+            this.exits.Location = new System.Drawing.Point(122, 126);
             this.exits.Name = "exits";
             this.exits.Size = new System.Drawing.Size(121, 24);
             this.exits.TabIndex = 3;
+            // 
+            // check
+            // 
+            this.check.Location = new System.Drawing.Point(101, 186);
+            this.check.Name = "check";
+            this.check.Size = new System.Drawing.Size(75, 23);
+            this.check.TabIndex = 4;
+            this.check.Text = "button3";
+            this.check.UseVisualStyleBackColor = true;
+            // 
+            // hide
+            // 
+            this.hide.Location = new System.Drawing.Point(43, 218);
+            this.hide.Name = "hide";
+            this.hide.Size = new System.Drawing.Size(171, 23);
+            this.hide.TabIndex = 5;
+            this.hide.Text = "Hide!";
+            this.hide.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.hide);
+            this.Controls.Add(this.check);
             this.Controls.Add(this.exits);
             this.Controls.Add(this.goThroughTheDoor);
             this.Controls.Add(this.goHere);
@@ -93,6 +115,8 @@
         private System.Windows.Forms.Button goHere;
         private System.Windows.Forms.Button goThroughTheDoor;
         private System.Windows.Forms.ComboBox exits;
+        private System.Windows.Forms.Button check;
+        private System.Windows.Forms.Button hide;
     }
 }
 
